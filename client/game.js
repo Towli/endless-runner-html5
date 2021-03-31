e8a3b1ce5e5cca6ef8fa2fbfa2b43e35973be75b
Alex Towli <alextowli@hotmail.co.uk>
2021-03-31 21:10:00 +0100

    feat: refactor and audio playback
    - use game state properly for lifecycle
    - audio playback on game start
    - OOP for draw logic

--- client/game.js
+++ client/game.js
@@ -2,10 +2,9 @@
 
 /**
  * todo:
- * 1. model player as a class
- * 2. promote draw and movement to objects
- * 3. cleanup collision logic
- * 4. fix collision clipping bug
+ * 1. promote movement to objects
+ * 2. cleanup collision logic
+ * 3. develop directional collision detection
  */
 
 const CONSTANTS = {
@@ -17,20 +16,21 @@ const CONSTANTS = {
     PLAYER_HEIGHT: 20,
     PLAYER_WIDTH: 20,
     GRAVITY: 70,
-    MAX_VELOCITY: 1000,
+    MAX_VELOCITY: 1200,
     DELTA_CEILING: 0.0167,
     DELTA_FLOOR: 0.1,
-    JUMP_SPEED: 800,
+    JUMP_SPEED: 1000,
 }
 
 const GAME_STATES = {
     WON: 1,
     PLAYING: 2,
     LOST: 3,
+    IDLE: 4,
 }
 
 const game = {
-    state: null,
+    state: GAME_STATES.IDLE,
 }
 
 const DELTA = {
@@ -40,15 +40,28 @@ const DELTA = {
 }
 
 const KEYCODES = {
-    JUMP: 32,
+    SPACE: 32,
 }
 
-class Player {
-    constructor(x, y, width, height) {
-        this.height = height
-        this.width = width
+class GameObject {
+    constructor(context, x, y, width, height) {
+        this.context = context
         this.x = x
         this.y = y
+        this.height = height
+        this.width = width
+    }
+
+    draw() {
+        this.context.fillStyle = this.colour
+        this.context.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+class Player extends GameObject {
+    constructor(context, x, y, width, height, colour = CONSTANTS.PLAYER_COLOUR) {
+        super(context, x, y, width, height)
+        this.colour = colour
         this.velocity = {
             x: 0,
             y: 0,
@@ -58,8 +71,10 @@ class Player {
     }
 }
 
-class Platform {
-    constructor(x, y, width, height) {
+class Platform extends GameObject {
+    constructor(context, x, y, width, height, colour = CONSTANTS.PLATFORM_COLOUR) {
+        super(context, x, y, width, height)
+        this.colour = colour
         this.height = height
         this.width = width
         this.x = x
@@ -76,20 +91,21 @@ function initialise() {
     context = canvas.getContext('2d')
 
     canvas.width = canvas.height = CONSTANTS.CANVAS_WIDTH
-    startMusic()
 
-    cleanStartGameLoop()
+    registerEventListeners()
+    renderMenuScreen()
 }
 
 function cleanStartGameLoop() {
     window.cancelAnimationFrame(animationFrameRequestId)
-    document.removeEventListener('keydown', handleRestart)
-    registerEventListeners()
+    startMusic()
 
-    player = new Player(50, 30, CONSTANTS.PLAYER_WIDTH, CONSTANTS.PLAYER_HEIGHT)
+    registerEventListeners()
+    player = new Player(context, 50, 30, CONSTANTS.PLAYER_WIDTH, CONSTANTS.PLAYER_HEIGHT)
+
     platforms = []
-    platforms.push(new Platform(0, 320, 250, 50))
-    platforms.push(new Platform(370, 300, 250, 50))
+    platforms.push(new Platform(context, 0, getRandomArbitrary(400, canvas.height - 50), 270, 50))
+    platforms.push(new Platform(context, 370, getRandomArbitrary(400, canvas.height - 50), 270, 50))
 
     game.state = GAME_STATES.PLAYING
 
@@ -144,6 +160,7 @@ function movePlatforms() {
     platforms.forEach((platform) => {
         if (platform.x + platform.width < 0) {
             platform.x = canvas.width
+            platform.y = getRandomArbitrary(300, canvas.height - 50)
         }
         platform.x -= platform.velocity * DELTA.value
     })
@@ -153,33 +170,16 @@ function draw() {
     context.fillStyle = CONSTANTS.BG_COLOUR
     context.fillRect(0, 0, canvas.width, canvas.height)
 
-    platforms.forEach((platform) => {
-        context.fillStyle = CONSTANTS.PLATFORM_COLOUR
-        context.fillRect(
-            platform.x,
-            platform.y,
-            platform.width,
-            platform.height
-        )
-    })
+    platforms.forEach((platform) => platform.draw())
 
-    context.fillStyle = CONSTANTS.PLAYER_COLOUR
-    context.fillRect(
-        player.x,
-        player.y,
-        CONSTANTS.PLAYER_WIDTH,
-        CONSTANTS.PLAYER_HEIGHT
-    )
+    player.draw()
 
     renderScore()
 }
 
 function updateDeltaTime() {
     DELTA.now = Date.now()
-    DELTA.value = Math.min(
-        (DELTA.now - DELTA.then) / 1000,
-        CONSTANTS.DELTA_CEILING
-    )
+    DELTA.value = Math.min((DELTA.now - DELTA.then) / 1000, CONSTANTS.DELTA_CEILING)
     DELTA.then = DELTA.now
 }
 
@@ -188,8 +188,14 @@ function registerEventListeners() {
 }
 
 function handleKeydown(e) {
-    if (e.keyCode === KEYCODES.JUMP) {
-        jump()
+    if (e.keyCode === KEYCODES.SPACE) {
+        if (game.state === GAME_STATES.PLAYING) {
+            return jump()
+        }
+
+        if (game.state === GAME_STATES.IDLE || game.state === GAME_STATES.LOST) {
+            return cleanStartGameLoop()
+        }
     }
 }
 
@@ -208,28 +214,23 @@ function updateGameState() {
 }
 
 function handleRestart(e) {
-    if (e.keyCode === KEYCODES.JUMP) {
+    if (e.keyCode === KEYCODES.SPACE) {
         cleanStartGameLoop()
     }
 }
 
 function endGame() {
-    document.removeEventListener('keydown', handleKeydown)
-    context.fillStyle = CONSTANTS.BG_COLOUR
-    context.fillRect(0, 0, canvas.width, canvas.height)
-    context.textAlign = 'center'
-    context.font = '24px Tahoma'
-    context.fillStyle = 'white'
-    context.fillText('game over', canvas.width / 2, canvas.height / 2)
-    context.font = '16px Tahoma'
-    context.fillText('<press space>', canvas.width / 2, canvas.height / 2 + 50)
-    document.addEventListener('keydown', handleRestart)
+    renderGameOverScreen()
 }
 
 function isColliding(player, platforms) {
     for (let i = 0; i < platforms.length; i++) {
         const collision = performAABBTest(
-            { ...player, width: player.width, height: player.height },
+            {
+                ...player,
+                width: player.width,
+                height: player.height,
+            },
             {
                 x: platforms[i].x,
                 y: platforms[i].y,
@@ -254,10 +255,35 @@ function isColliding(player, platforms) {
     }
 }
 
+function renderMenuScreen() {
+    document.addEventListener('keydown', handleKeydown)
+    context.fillStyle = CONSTANTS.BG_COLOUR
+    context.fillRect(0, 0, canvas.width, canvas.height)
+    context.textAlign = 'center'
+    context.font = '24px Tahoma'
+    context.fillStyle = 'black'
+    context.fillText('runna', canvas.width / 2, canvas.height / 3)
+    context.font = '16px Tahoma'
+    context.fillText(`space to start`, canvas.width / 2, canvas.height / 2)
+}
+
+function renderGameOverScreen() {
+    context.fillStyle = CONSTANTS.BG_COLOUR
+    context.fillRect(0, 0, canvas.width, canvas.height)
+    context.textAlign = 'center'
+    context.font = '24px Tahoma'
+    context.fillStyle = 'black'
+    context.fillText('game over', canvas.width / 2, canvas.height / 3)
+    context.font = '16px Tahoma'
+    context.fillText(`score: ${player.score}`, canvas.width / 2, canvas.height / 2)
+    context.font = '16px Tahoma'
+    context.fillText('[press space]', canvas.width / 2, canvas.height / 1.5)
+}
+
 function renderScore() {
     context.font = '16px Tahoma'
-    context.fillStyle = 'white'
-    context.fillText(player.score, 15, 20)
+    context.fillStyle = 'black'
+    context.fillText(player.score, 30, 20)
 }
 
 function updateScore() {
@@ -265,7 +291,11 @@ function updateScore() {
 }
 
 function startMusic() {
-    // todo
+    document.getElementById('audio').play().catch(console.log)
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min
 }
 
 initialise()
